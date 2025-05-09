633c7ae8477f0366201397da872cfd387e66c326
Fauzan Nurdiansyah <fauzannurdiansyah13@gmail.com>
2025-05-09 16:33:47 +0700

    update dan fix routenya

--- app/src/main/java/com/fauzan0111/fauzan_sleepquality/nav/Screen.kt
+++ app/src/main/java/com/fauzan0111/fauzan_sleepquality/nav/Screen.kt
@@ -1,11 +1,14 @@
 package com.fauzan0111.fauzan_sleepquality.nav
 
+import com.fauzan0111.fauzan_sleepquality.screens.KEY_ID_DATA
+
 sealed class Screen(val route: String) {
     data object Main : Screen("main")
-    data object TambahDataTidur : Screen("tambah")
+    data object ListScreen : Screen ("list")
+    data object TambahDataTidur : Screen("edit")
     data object Info : Screen("info")
     data object About : Screen("about")
-    object EditDataTidur : Screen("edit/{id}") {
+    data object EditDataTidur : Screen("edit/{$KEY_ID_DATA}") {
         fun withId(id: Long): String {
             return "edit/$id"
         }
--- app/src/main/java/com/fauzan0111/fauzan_sleepquality/nav/SleepNavGraph.kt
+++ app/src/main/java/com/fauzan0111/fauzan_sleepquality/nav/SleepNavGraph.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.fauzan0111.fauzan_sleepquality.screens.AboutScreen
 import com.fauzan0111.fauzan_sleepquality.screens.InfoScreen
+import com.fauzan0111.fauzan_sleepquality.screens.KEY_ID_DATA
 import com.fauzan0111.fauzan_sleepquality.screens.MainScreen
 import com.fauzan0111.fauzan_sleepquality.screens.ResultScreen
 import com.fauzan0111.fauzan_sleepquality.screens.SleepListScreen
@@ -23,18 +24,19 @@ fun NavGraph(navController: NavHostController) {
             MainScreen(navController = navController)
         }
         composable(route = Screen.TambahDataTidur.route) {
+            ResultScreen(navController = navController)
+        }
+        composable(route = Screen.ListScreen.route) {
             SleepListScreen(navController = navController)
         }
         composable(
             route = Screen.EditDataTidur.route,
-            arguments = listOf(
-                navArgument("id") {
-                    type = NavType.LongType
-                }
-            )
+            arguments = listOf(navArgument(KEY_ID_DATA) {
+                type = NavType.LongType
+            })
         ) { backStackEntry ->
-            val id = backStackEntry.arguments?.getLong("id") ?: return@composable
-            ResultScreen(navController = navController, id = id)
+            val id = backStackEntry.arguments?.getLong(KEY_ID_DATA) ?: return@composable
+            ResultScreen(id,navController)
         }
 
         composable(route = Screen.About.route){
