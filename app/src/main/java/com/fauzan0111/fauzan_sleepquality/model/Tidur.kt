aec5560a47801c04560a0777a388e026ed46dc37
Fauzan Nurdiansyah <fauzannurdiansyah13@gmail.com>
2025-06-13 01:54:17 +0700

    membuat model tidur

---
+++ app/src/main/java/com/fauzan0111/fauzan_sleepquality/model/Tidur.kt
@@ -0,0 +1,9 @@
+package com.fauzan0111.fauzan_sleepquality.model
+
+data class Tidur(
+    val id: String,
+    val waktu_tidur: String,
+    val waktu_bangun: String,
+    val imageId: String,
+    val mine: Int
+)
